6a6e8b4ddd161495ddffbf750424e0aed1db8210
GaryM636 <142124124+GaryM636@users.noreply.github.com>
2023-09-17 15:48:48 -0700

    added provided index and style files, added script.js

---
+++ script.js
@@ -0,0 +1,137 @@
+var generateBtn = document.querySelector("#generate");
+
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.',
+];
+
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+var lowerCasedCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+];
+
+var upperCasedCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+];
+
+function getPasswordLength() {
+  var length;
+  for (;;) {
+    length = Number(prompt("Choose how many characters long you'd like your password to be (between 8-128 characters): "));
+    if (!isNaN(length) && length >= 8 && length <= 128) {
+      break;
+    }
+    alert("Password length must be a number between 8-128 characters");
+  }
+  return length;
+}
+
+function confirmCharacterType(message) {
+  var input;
+  for (;;) {
+    input = prompt(message).toLowerCase();
+    if (input === "yes" || input === "no") {
+      break;
+    }
+    alert("Please answer Yes or No");
+  }
+  return input === "yes";
+}
+
+function generatePassword() {
+  var length = getPasswordLength();
+  var useUppercase = confirmCharacterType("Do you want to include uppercase letters in your password? (Yes or No)");
+  var useNumbers = confirmCharacterType("Do you want to include numbers in your password? (Yes or No)");
+  var useSpecialChars = confirmCharacterType("Do you want to include special characters in your password? (Yes or No)");
+
+  var characters = lowerCasedCharacters;
+  if (useUppercase) characters = characters.concat(upperCasedCharacters);
+  if (useNumbers) characters = characters.concat(numericCharacters);
+  if (useSpecialChars) characters = characters.concat(specialCharacters);
+
+  var password = "";
+  for (var i = 0; i < length; i++) {
+    password += characters[Math.floor(Math.random() * characters.length)];
+  }
+  return password;
+}
+
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+
+generateBtn.addEventListener("click", writePassword);
